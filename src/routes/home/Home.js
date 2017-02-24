34fd143b2b9ff8981715adcb961a1c3765454a77
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2017-02-23 23:23:40 -0500

    add home image

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -20,6 +20,7 @@ class Home extends Component {
     return (
       <div className={styles.supremeContainer}>
         <HomeAbstract />
+        <div className={styles.image} />
         <Demo />
         <HomeStory />
         <HomeBottomSignup />
